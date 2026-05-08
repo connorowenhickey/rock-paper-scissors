598c490993f6127b27c2de7017a7bd835dca5f40
connorowenhickey <185983925+connorowenhickey@users.noreply.github.com>
2026-05-09 00:52:21 +0100

    Game runs off buttons, not typed inputs
    Refactored code with new UI. Buttons are
    easier to play with than typing words. Used
    JS to create dynamic webpage.

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -4,7 +4,7 @@
 //Assign the various numbers to each possible option
 
 function getComputerChoice(){
-    x = Math.floor(Math.random()*3);
+    let x = Math.floor(Math.random()*3);
     if (x===0){
         return 'rock'
     } else if (x===1) {
@@ -14,65 +14,66 @@ function getComputerChoice(){
     } 
 }
 
-//while loop to force user to give valid input
-//choice variable is assigned in this loop
-//input to give interactive popup to type in choice
-function getHumanChoice() {
-    let keepGoing = true
-    while (keepGoing) {
-        let choice = prompt('Rock, Paper or Scissors').toLowerCase()
-        if (['rock', 'paper', 'scissors'].includes(choice)) {
-            return choice
-            keepGoing = false
-        }    
-    }
-}
 
 
 //if elseif else loop for when there is draw, win or loss for human
 //with updated scores and a log update 
 function playRound(computerChoice, humanChoice){
+    const p = document.createElement('p')
+    const para_end = document.createElement('p')
     if (computerChoice === humanChoice) {
-        console.log(`Draw! - Scores remain: computer - ${computerScore} and human - ${humanScore}`)
+        p.textContent = `Draw! - Scores remain: Computer - ${computerScore} and Human - ${humanScore}`
     } else if ((computerChoice === 'rock' && humanChoice === 'scissors') ||
     (computerChoice === 'paper' && humanChoice === 'rock') ||
     (computerChoice === 'scissors' && humanChoice === 'paper')) {
         computerScore += 1
-        console.log(`Computer wins! The score is now: Computer - ${computerScore} and Human - ${humanScore}`)
+        p.textContent = `Computer wins! The score is now: Computer - ${computerScore} and Human - ${humanScore}`
     } else {
         humanScore += 1
-        console.log(`Human wins! The score is now: Computer - ${computerScore} and Human - ${humanScore}`)
+        p.textContent = `Human wins! The score is now: Computer - ${computerScore} and Human - ${humanScore}`
+    }
+
+    roundResult.appendChild(p) 
+
+    if (humanScore === 5) {
+        para_end.textContent = `Human wins! The game is over! Reload Window to restart.`
+        rock.disabled = true
+        paper.disabled = true
+        scissors.disabled = true
+        results.appendChild(para_end)
+    } else if (computerScore === 5) {
+        para_end.textContent = `Computer wins. The game is over! Reload Window to restart.`
+        rock.disabled = true
+        paper.disabled = true
+        scissors.disabled = true
+        results.appendChild(para_end)
     }
 }
 
-//multiple rounds, have to define human and computer choice
-//have to play game with chosen values
-//reset gam each time
-//finish game after number of rounds played
-function playGame(rounds) {
-    for (let i = 0; i < rounds; i++) {
-        let humanChoice = getHumanChoice()
-        console.log(humanChoice)
-        let computerChoice = getComputerChoice()
-        console.log(computerChoice)
-        playRound(computerChoice, humanChoice)
-    }
-    if (humanScore > computerScore) {
-        console.log(`Congrats! You won the game ${humanScore}-${computerScore}`)
 
-    } else if (computerScore > humanScore) {
-        console.log(`Unlucky loser! You lost ${computerScore} - ${humanScore}`)
-    } else {
-        console.log(`It's a draw! Rock, Paper, Scissors is the real winner!`)
-    }
-}
-
-//initalise variable for human score
+//initilize human score
 let humanScore = 0
-
-//intialise variable for computer score
+//initialise computer score
 let computerScore = 0
 
-let rounds = prompt('How many rounds do you want to play?')
+let computerChoice = getComputerChoice()
 
-playGame(rounds)
+const results = document.querySelector('.results')
+const roundResult = document.querySelector('.round-result')
+
+const rock = document.querySelector('#rock-btn')
+const paper = document.querySelector('#paper-btn')
+const scissors = document.querySelector('#scissors-btn')
+
+rock.addEventListener('click', () => {
+    playRound(getComputerChoice(), 'rock')
+})
+
+paper.addEventListener('click', () => {
+    playRound(getComputerChoice(), 'paper')
+})
+
+scissors.addEventListener('click', () => {
+    playRound(getComputerChoice(), 'scissors')
+})
+
